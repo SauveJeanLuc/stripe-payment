e73adb574bdb0398794774399e2637e230b75d4c
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-01-27 15:31:07 +0200

    Refactor with Parameterized Test

--- src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
+++ src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
@@ -3,6 +3,9 @@ package com.amigoscode.testing.utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -13,6 +16,18 @@ public class PhoneNumberValidatorTest {
     @BeforeEach
     void setUp() {
         underTest = new PhoneNumberValidator();
+    }
+
+    @ParameterizedTest
+    //Pass Actual input and Expected Value
+    @CsvSource({
+            "+250798000000 , true",
+            "+25079800000078913, false",
+            "250798000000789, false"
+    })
+    void itShouldValidatePhoneNumberSuccess(String phoneNumber, boolean expected) {
+        boolean isValid = underTest.test(phoneNumber);
+        assertThat(isValid).isEqualTo(expected);
     }
 
     @Test
