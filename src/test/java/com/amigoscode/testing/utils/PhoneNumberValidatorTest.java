b97c9cc9db6bb0e869c30b70f5975e164604ad67
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-01-27 15:07:41 +0200

    Should fail when length is Bigger Than 13 ~ Test

--- src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
+++ src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
@@ -1,7 +1,10 @@
 package com.amigoscode.testing.utils;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class PhoneNumberValidatorTest {
 
@@ -9,13 +12,32 @@ public class PhoneNumberValidatorTest {
 
     @BeforeEach
     void setUp() {
-        underTest = new PhoneNumberValidatorTest();
+        underTest = new PhoneNumberValidator();
     }
 
     @Test
     void itShouldValidatePhoneNumber(){
         //Given
+        String phoneNumber = "+250798000000";
+
         //When
+        boolean isValid = underTest.test(phoneNumber);
+
         //Then
+        assertThat(isValid).isTrue();
     }
+
+    @Test
+    @DisplayName("Should fail when length is Bigger Than 13")
+    void itShouldValidatePhoneNumberWhenIncorrectAndHasLengthBiggerThan13(){
+        //Given
+        String phoneNumber = "+250798000000789";
+
+        //When
+        boolean isValid = underTest.test(phoneNumber);
+
+        //Then
+        assertThat(isValid).isTrue();
+    }
+
 }
