015ef2c3b94918c3e3df0d39604cf7be46adc16c
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-01-27 12:12:38 +0200

    Wrote Test before Actual class creation

---
+++ src/test/java/com/amigoscode/testing/utils/PhoneNumberValidatorTest.java
@@ -0,0 +1,21 @@
+package com.amigoscode.testing.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PhoneNumberValidatorTest {
+
+    private PhoneNumberValidator underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new PhoneNumberValidatorTest();
+    }
+
+    @Test
+    void itShouldValidatePhoneNumber(){
+        //Given
+        //When
+        //Then
+    }
+}
